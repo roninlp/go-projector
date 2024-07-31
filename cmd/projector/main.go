48a751d10e9b7e1bb1c3066d5e9d36dd86f11dd0
ronin <ronin.lp@gmail.com>
2024-08-01 00:17:10 +0330

    go cli done

--- cmd/projector/main.go
+++ cmd/projector/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
@@ -18,5 +19,27 @@ func main() {
 		log.Fatalf("unable to get config %v", err)
 	}
 
-	fmt.Printf("opts: %+v ", config)
+	proj := cli.NewProjector(config)
+	if config.Operation == cli.Print {
+		if len(config.Args) == 0 {
+			data := proj.GetValueAll()
+			jsonString, err := json.Marshal(data)
+			if err != nil {
+				log.Fatalf("this line should never be reached %v", err)
+			}
+
+			fmt.Printf("%v", string(jsonString))
+		} else if value, ok := proj.GetValue(config.Args[0]); ok {
+			fmt.Printf("%v", value)
+		}
+	}
+
+	if config.Operation == cli.Add {
+		proj.SetValue(config.Args[0], config.Args[1])
+		proj.Save()
+	}
+	if config.Operation == cli.Remove {
+		proj.RemoveValue(config.Args[0])
+		proj.Save()
+	}
 }
